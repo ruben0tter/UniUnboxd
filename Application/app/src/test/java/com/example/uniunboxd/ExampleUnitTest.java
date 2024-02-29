6b0276f0a0562bb799cae45a008a2e29a4855959
Iluminuss <m.svilenski@gmail.com>
2024-02-29 16:01:39 +0100

    Change name of the app

--- Application/app/src/test/java/com/example/uniunboxd/ExampleUnitTest.java
+++ Application/app/src/test/java/com/example/uniunboxd/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.example.uniunboxd;
 
 import org.junit.Test;
 
--- Application/settings.gradle.kts
+++ Application/settings.gradle.kts
@@ -13,6 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "app"
+rootProject.name = "UniUnboxd"
 include(":app")
- 
