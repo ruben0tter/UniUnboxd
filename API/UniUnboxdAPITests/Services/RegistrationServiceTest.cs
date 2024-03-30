a3a799366ed3e6bbd096eb0358b8ef3d1477f6ac
Ruben Otter <ruben.otter3@gmail.com>
2024-03-30 21:15:30 +0100

    RegistrationService Testing

---
+++ API/UniUnboxdAPITests/Services/RegistrationServiceTest.cs
@@ -0,0 +1,125 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage;
+using UniUnboxdAPI.Models;
+using UniUnboxdAPITests.TestUtilities;
+
+namespace UniUnboxdAPITests.Services
+{
+    [TestClass]
+    public class RegistrationServiceTest
+    {
+        private readonly RegistrationService registrationService;
+        private readonly UniUnboxdDbContext dbContext;
+        private readonly UserManager<User> userManager;
+
+        public RegistrationServiceTest()
+        {
+            dbContext = DatabaseUtil.CreateDbContext();
+            userManager = DatabaseUtil.CreateUserManager(dbContext);
+            var mailService = new MailService(ConfigurationUtil.CreateConfiguration());
+            registrationService = new RegistrationService(userManager, mailService);
+        }
+
+        [TestMethod]
+        public void CreateStudentUserTest()
+        {
+            var model = new RegisterModel() { Email = "student@gmail.com", Password = "test", Type = UserType.Student };
+            var user = registrationService.CreateUser(model);
+
+            Assert.IsInstanceOfType(user, typeof(Student));
+            Assert.AreEqual(user.UserName, "student");
+            Assert.AreEqual(user.Email, "student@gmail.com");
+            Assert.AreEqual(user.UserType, UserType.Student);
+            Assert.IsNotNull(user.SecurityStamp);
+            Assert.IsNotNull(user.CreationTime);
+            Assert.IsNotNull(user.LastModificationTime);
+            Assert.IsNotNull((user as Student)!.NotificationSettings);
+        }
+
+        [TestMethod]
+        public void CreateUniversityUserTest()
+        {
+            var model = new RegisterModel() { Email = "university@gmail.com", Password = "test", Type = UserType.University };
+            var user = registrationService.CreateUser(model);
+
+            Assert.IsInstanceOfType(user, typeof(University));
+            Assert.AreEqual(user.UserName, "university");
+            Assert.AreEqual(user.Email, "university@gmail.com");
+            Assert.AreEqual(user.UserType, UserType.University);
+            Assert.IsNotNull(user.SecurityStamp);
+            Assert.IsNotNull(user.CreationTime);
+            Assert.IsNotNull(user.LastModificationTime);
+        }
+
+        // TODO: Implement Professor Creation.
+        /**
+        [TestMethod]
+        public void CreateProfessorUserTest()
+        {
+            var model = new RegisterModel() { Email = "professor@gmail.com", Password = "test", Type = UserType.Professor };
+            var user = registrationService.CreateUser(model);
+
+            Assert.IsInstanceOfType(user, typeof(Professor));
+            Assert.AreEqual(user.UserName, "professor");
+            Assert.AreEqual(user.Email, "professor@gmail.com");
+            Assert.AreEqual(user.UserType, UserType.Professor);
+            Assert.IsNotNull(user.SecurityStamp);
+            Assert.IsNotNull(user.CreationTime);
+            Assert.IsNotNull(user.LastModificationTime);
+        }
+        */
+
+        [TestMethod]
+        public void IsEmailValidTestValid()
+        {
+            var email = "valid@gmail.com";
+            var result = registrationService.IsEmailValid(email);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsEmailValidTestUnvalid()
+        {
+            var email = "invalid.gmail.com";
+            var result = registrationService.IsEmailValid(email);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task DoesEmailExistTestWithNonExistingEmail()
+        {
+            var email = "nonexisting@gmail.com";
+            var result = await registrationService.DoesEmailExist(email);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task DoesEmailExistTestWithExistingEmail()
+        {
+            // Create account.
+            var model = new RegisterModel() { Email = "existing@gmail.com", Password = "test", Type = UserType.Student };
+            var user = registrationService.CreateUser(model);
+            await userManager.CreateAsync(user);
+
+            var result = await registrationService.DoesEmailExist(model.Email);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public async Task CreateAccountTest()
+        {
+            var model = new RegisterModel() { Email = "test@gmail.com", Password = "test", Type = UserType.Student };
+            var user = registrationService.CreateUser(model);
+            var result = await registrationService.CreateAccount(user, model.Password);
+
+            var isCreated = await registrationService.DoesEmailExist(model.Email);
+
+            Assert.IsTrue(result);
+            Assert.IsTrue(isCreated);
+        }
+    }
+}
