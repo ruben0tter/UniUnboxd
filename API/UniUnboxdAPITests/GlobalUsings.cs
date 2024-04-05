1ecf516ecb6067604def04307652cd6a17b471f1
Ruben Otter <ruben.otter3@gmail.com>
2024-04-05 17:56:24 +0200

    HttpContext Configuration

--- API/UniUnboxdAPITests/GlobalUsings.cs
+++ API/UniUnboxdAPITests/GlobalUsings.cs
@@ -1,4 +1,5 @@
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using UniUnboxdAPI.Controllers;
 global using UniUnboxdAPI.Data;
 global using UniUnboxdAPI.Models;
 global using UniUnboxdAPI.Models.DataTransferObjects;
--- API/UniUnboxdAPITests/TestUtilities/ConfigurationUtil.cs
+++ API/UniUnboxdAPITests/TestUtilities/ConfigurationUtil.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+using UniUnboxdAPI.Models;
 
 namespace UniUnboxdAPITests.TestUtilities
 {
@@ -14,6 +18,19 @@ namespace UniUnboxdAPITests.TestUtilities
             return new ConfigurationBuilder()
                 .AddInMemoryCollection(config)
                 .Build();
+        }
+
+        public static void SetHttpContext(ControllerBase controller, int id, UserType role)
+        {
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.NameIdentifier, id.ToString()),
+                new(ClaimTypes.Role, role.ToString())
+            };
+            var identity = new ClaimsIdentity(claims);
+            var claimsPrincipal = new ClaimsPrincipal(identity);
+
+            controller.ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = claimsPrincipal } };
         }
     }
 }
