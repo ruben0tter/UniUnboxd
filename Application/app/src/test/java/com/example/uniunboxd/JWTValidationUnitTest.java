eecdb034ce6feed4c2dcd5c8f3ba5d7a6255fdf2
Raul Hernandez <me@spaceface.dev>
2024-04-05 13:48:52 +0200

    fix jwt test

--- Application/app/src/test/java/com/example/uniunboxd/JWTValidationUnitTest.java
+++ Application/app/src/test/java/com/example/uniunboxd/JWTValidationUnitTest.java
@@ -13,20 +13,18 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import com.example.uniunboxd.utilities.JWTValidation;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
-public class JWTValidationUnitTest
-{
-    private static Context ctx;
+public class JWTValidationUnitTest {
+    private Context ctx;
 
     static final String TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
-    @BeforeClass
-    public static void setup() {
+    @Before
+    public void setup() {
         ctx = InstrumentationRegistry.getInstrumentation().getContext();
     }
 
@@ -41,19 +39,19 @@ public class JWTValidationUnitTest
 
     @Test
     public void useToken() {
+        JWTValidation.placeToken(TOKEN, ctx);
         assertEquals(TOKEN, JWTValidation.getToken(ctx));
-        assertEquals( "1234567890", JWTValidation.getTokenProperty(ctx, "sub"));
+        assertEquals("1234567890", JWTValidation.getTokenProperty(ctx, "sub"));
         assertEquals("John Doe", JWTValidation.getTokenProperty(ctx, "name"));
     }
 
     @Test
     public void logout() {
+        JWTValidation.placeToken(TOKEN, ctx);
         assertNotNull(JWTValidation.getToken(ctx));
         assertTrue(JWTValidation.isUserLoggedIn(ctx));
         JWTValidation.deleteToken(ctx);
         assertFalse(JWTValidation.isUserLoggedIn(ctx));
         assertNull(JWTValidation.getToken(ctx));
     }
-
-
 }
